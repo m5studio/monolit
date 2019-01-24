1b810d02047fd9acae57d27a88f917a0d4cdc99a
DeN <jquphp@gmail.com>
2019-01-24 11:32:20 +0300

    added main slider

--- layout/src/boostrap/boostrap.js
+++ layout/src/boostrap/boostrap.js
@@ -1,6 +1,8 @@
 // import 'bootstrap'
 
-// import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/carousel';
+
 // import 'bootstrap/js/dist/alert';
 // import 'bootstrap/js/dist/modal';
 // ...
--- layout/src/main.js
+++ layout/src/main.js
@@ -3,22 +3,22 @@ import "./main.scss"
 import "./boostrap/boostrap"
 import {stickyMainNav} from "./core/main_nav/main_nav"
 
-// import 'nouislider'
+import 'nouislider'
 
 
 $(document).ready(function() {
     // console.log("document ready function")
 
     // Init test noUiSlider
-    // const squareSlider = document.getElementById('square-slider');
-    // noUiSlider.create(squareSlider, {
-    //     start: [20, 80],
-    //     connect: true,
-    //     range: {
-    //         'min': 0,
-    //         'max': 100
-    //     }
-    // });
+    const realtySquareSlider = document.getElementById('realty-filter__square-slider');
+    noUiSlider.create(realtySquareSlider, {
+        start: [35, 245],
+        connect: true,
+        range: {
+            'min': 35,
+            'max': 245
+        }
+    });
 })
 
 
