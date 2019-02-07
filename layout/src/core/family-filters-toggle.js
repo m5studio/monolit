37927280a85563d301db650e78e68a7fdf21f9c4
DeN <jquphp@gmail.com>
2019-02-07 19:40:13 +0300

    oops

--- layout/src/core/family-filters-toggle.js
+++ layout/src/core/family-filters-toggle.js
@@ -1,6 +1,5 @@
 function familyFiltersToggle() {
-    $('#section-family-types-filters__top').click(function(event) {
-        event.preventDefault()
+    $('#section-family-types-filters__top').click(function() {
         $('#section-family-types-filters__inner').toggleClass('display-grid-family-filters')
     })
 }
