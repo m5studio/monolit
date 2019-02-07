e566a5cd2e4923646ff50ee6a028cca726e2d1ed
DeN <jquphp@gmail.com>
2019-02-07 19:38:03 +0300

    few features for section-family-types-filters

--- layout/src/core/family-filters-toggle.js
+++ layout/src/core/family-filters-toggle.js
@@ -1,5 +1,6 @@
 function familyFiltersToggle() {
-    $('#section-family-types-filters__top').click(function() {
+    $('#section-family-types-filters__top').click(function(event) {
+        event.preventDefault()
         $('#section-family-types-filters__inner').toggleClass('display-grid-family-filters')
     })
 }
