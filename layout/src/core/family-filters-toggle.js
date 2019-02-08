cb8673e55d61b8ce5cb6655e4d7edd99af5ff640
DeN <jquphp@gmail.com>
2019-02-08 13:13:41 +0300

    #section-family-types-filters correct hide\show behaviour

--- layout/src/core/family-filters-toggle.js
+++ layout/src/core/family-filters-toggle.js
@@ -1,6 +1,20 @@
 function familyFiltersToggle() {
-    $('#section-family-types-filters__top').click(function() {
-        $('#section-family-types-filters__inner').toggleClass('display-grid-family-filters')
+
+    const top_selector = $('#section-family-types-filters__top')
+    const inner_selector = $('#section-family-types-filters__inner')
+    const class_display = 'display-grid-family-filters'
+
+    // hide #section-family-types-filters__inner when clicked outside #section-family-types-filters__top and #section-family-types-filters__inner
+    $(document).mouseup(function(e) {
+        if (!top_selector.is(e.target) && top_selector.has(e.target).length === 0
+            && !inner_selector.is(e.target) && inner_selector.has(e.target).length === 0 )
+        {
+            inner_selector.removeClass(class_display);
+        }
+    })
+
+    top_selector.on('click', () => {
+        inner_selector.toggleClass(class_display);
     })
 }
 
