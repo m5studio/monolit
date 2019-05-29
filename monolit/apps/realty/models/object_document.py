c09a7781e72ca35afa79866437ccdf5283896999
DeN <jquphp@gmail.com>
2019-05-29 16:16:04 +0300

    object video

--- monolit/apps/realty/models/object_document.py
+++ monolit/apps/realty/models/object_document.py
@@ -1,7 +1,7 @@
 import datetime
 from django.db import models
 
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
 from apps.core.classes.clean_media import CleanMedia
--- monolit/apps/realty/models/object_video.py
+++ monolit/apps/realty/models/object_video.py
@@ -1,4 +1,8 @@
 from django.db import models
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from apps.realty.models.object import Object
 
 
@@ -6,7 +10,7 @@ class ObjectVideo(models.Model):
     object    = models.ForeignKey(Object, on_delete=models.CASCADE, default=0)
     title     = models.CharField('Заголовок видео', max_length=255, blank=True, null=True)
     video_url = models.URLField('Cсылка на видео в YouTube', blank=True, null=True, help_text='e.g.: https://youtu.be/JbacFR_B-jw')
-    created   = models.DateTimeField(auto_now=False, auto_now_add=True, blank=True, null=True)
+    # created   = models.DateTimeField(auto_now=False, auto_now_add=True, blank=True, null=True)
     updated   = models.DateTimeField(auto_now=True, auto_now_add=False, blank=True, null=True)
 
     # def __str__(self):
@@ -15,3 +19,10 @@ class ObjectVideo(models.Model):
     class Meta:
         verbose_name = 'Видео'
         verbose_name_plural = 'Видео'
+
+
+@receiver(post_save, sender=ObjectVideo)
+def get_youtube_video_code(sender, instance, **kwargs):
+    import re
+    if instance.video_url:
+        print(instance.video_url)
