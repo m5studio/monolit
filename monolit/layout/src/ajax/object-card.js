5db15c45cc4841739bb4c28335ca3b07538412f1
DeN <jquphp@gmail.com>
2019-06-26 11:44:45 +0300

    js numbers format

--- monolit/layout/src/ajax/object-card.js
+++ monolit/layout/src/ajax/object-card.js
@@ -1,3 +1,13 @@
+function formaThousands(number, precision=0) {
+    // number = parseInt(number, 10).toFixed(precision)
+    number = parseFloat(number).toFixed(precision)
+    if (isNaN(number)) {
+        number = ''
+    }
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+}
+
+
 function objectCardSitesInfo() {
     $('.object-card').each((index, el) => {
         let site_info_url = $(el).data('sites-info-url')
@@ -5,14 +15,10 @@ function objectCardSitesInfo() {
         $.getJSON(site_info_url, (data) => {
             $.each(data, (index, val) => {
                 $(el).find('.oc-total-flats-qty').html( val['object_total_sites_qty'] )
+                $(el).find('.oc-min-price').append( formaThousands(val['object_min_site_price']) )
 
-                let site_price = val['object_min_site_price']
-                // let site_price = parseInt(val['object_min_site_price'], 10)
-                // let site_price = parseInt(val['object_min_site_price'], 10).toString()
-                // let site_price = parseFloat(val['object_min_site_price'], 1).toString()
-                // $(el).find('.oc-min-price').append( site_price.replace(/\B(?=(\d{3})+(?!\d))/g, " ") )
-                // $(el).find('.oc-min-price').append( site_price.replace(/\B(?=(\d{3})+(?!\d))/g, " ").replace('NaN','') )
-                $(el).find('.oc-min-price').append( site_price )
+                $(el).find('.oc-flats-area-min').append( formaThousands(val['object_min_site_area'], 1) )
+                $(el).find('.oc-flats-area-max').append( formaThousands(val['object_max_site_area'], 1) )
             })
         })
     })
