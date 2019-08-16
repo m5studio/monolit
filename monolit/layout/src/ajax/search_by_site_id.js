d8f8f9aa57610406cf118df519d049387028ee66
DeN <jquphp@gmail.com>
2019-08-16 20:41:23 +0300

    search by id

--- monolit/layout/src/ajax/search_by_site_id.js
+++ monolit/layout/src/ajax/search_by_site_id.js
@@ -1,10 +1,35 @@
-function search_by_site_id() {
-    console.log('search_by_site_id')
+function preventPressEnter() {
+    $(window).keydown((event) => {
+        if (event.keyCode == 13) {
+            event.preventDefault()
+            return false
+        }
+    })
+}
 
-    const all_sites_api = $('#search-by-site-id').data('api-all-sites') 
+
+function search_by_site_id() {
+    const search_id = $('#search-by-site-id')
+    const search_results_id = $('#search-by-site-id__results')
+    const all_sites_api = search_id.data('api-all-sites')
+
+    preventPressEnter()
 
     $('#search-by-site-id').keyup((event) => {
-        console.log('keyup ' + all_sites_api)
+        // search_results_id.empty()
+        search_results_id.html('')
+
+        let serch_val = search_id.val()
+        let expression = new RegExp(serch_val, "i")
+
+        $.getJSON(all_sites_api, function(data) {
+            $.each(data, function(index, el) {
+                // console.log(el['id'] + ' ' + el['crm_id'])
+                if (el.crm_id.search(expression) != -1) {
+                    search_results_id.append('<div>' + el['crm_id'] + '</div>')
+                }
+            })
+        })
     })
 }
 
