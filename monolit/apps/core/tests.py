6042c0585452e781f21663e75a06701888746e77
Denis <jquphp@gmail.com>
2019-12-02 22:00:05 +0300

    tests

--- monolit/apps/core/tests.py
+++ monolit/apps/core/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+# from django.test import TestCase
 
 # Create your tests here.
+# ==============================================
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+
+
+# class CoreTests(TestCase):
+# class CoreTests(SimpleTestCase):
+#     def test_sample(self):
+#         assert 1 == 1
+#
+#     def test_homepage_status_code(self):
+#         # response = self.client.get(reverse('homepage'))
+#         response = self.client.get('/')
+#
+#         print( response.status_code )
+#         # response = self.client.get(reverse('homepage'))
+#         # response = self.client.get('/')
+#         # self.assertEqual(response.status_code, 200)
+#         # self.assertEqual(201, 200)
+
+
+class TestCore(TestCase):
+# class TestCore(SimpleTestCase):
+
+    def test_sample(self):
+        assert 1 == 1
+
+    def test_homepage(self):
+        # print(reverse('homepage'))
+        # response = self.client.get('/')
+        response = self.client.get(reverse('homepage'))
+        self.assertEqual(response.status_code, 200)
--- monolit/apps/realty/tests.py
+++ monolit/apps/realty/tests.py
@@ -1,35 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-# ==============================================
-from django.test import TestCase, SimpleTestCase
-# from django.urls import reverse
-
-
-# class CoreTests(TestCase):
-# class CoreTests(SimpleTestCase):
-#     def test_sample(self):
-#         assert 1 == 1
-#
-#     def test_homepage_status_code(self):
-#         # response = self.client.get(reverse('homepage'))
-#         response = self.client.get('/')
-#
-#         print( response.status_code )
-#         # response = self.client.get(reverse('homepage'))
-#         # response = self.client.get('/')
-#         # self.assertEqual(response.status_code, 200)
-#         # self.assertEqual(201, 200)
-
-
-# class TestCore(TestCase):
-class TestCore(SimpleTestCase):
-    def test_sample(self):
-        assert 1 == 1
-
-    # def test_homepage(self):
-    #     client = Client()
-    #     # response = client.get(reverse('homepage'))
-    #     response = client.get('/')
-    #     self.assertEquals(response.status_code, 200)
-    #     # self.assertTemplateUsed('homepage/homepage.html')
