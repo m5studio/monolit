01f572f3ee3b0ce4b5f4d98ccb83b42fc270832a
Denis <jquphp@gmail.com>
2019-12-02 18:17:02 +0300

    django update & tests

--- monolit/apps/core/tests.py
+++ monolit/apps/core/tests.py
@@ -1,20 +1,3 @@
-from django.test import TestCase, SimpleTestCase
-# from django.urls import reverse
+from django.test import TestCase
 
-
-# class CoreTests(TestCase):
-class CoreTests(SimpleTestCase):
-    def test_homepage_status_code(self):
-        # response = self.client.get('/')
-        # print(response.status_code)
-        # self.assertEqual(response.status_code, 200)
-
-        # url = reverse('homepage')
-        # print(url)
-
-        assert 1 == 1
-
-
-    # def test_response_status(self):
-    #     response = self.client.get(reverse('homepage'))
-    #     self.assertEqual(response.status_code, 200)
+# Create your tests here.
