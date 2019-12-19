9a5309b4a272f4b02d08cfbbf86651860d1b16d4
Denis <jquphp@gmail.com>
2019-12-19 13:56:29 +0300

    Update settings.py

--- monolit/config/settings.py
+++ monolit/config/settings.py
@@ -175,10 +175,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'layout/dist'),
 
     ('css', os.path.join(BASE_DIR, 'layout/dist/css')),
