ca0881b3a9b5a7069a760d3bb3eb3969a8145496
Denis <jquphp@gmail.com>
2019-12-19 13:07:41 +0300

    Update settings.py

--- monolit/config/settings.py
+++ monolit/config/settings.py
@@ -127,8 +127,8 @@ if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': env("DB_NAME"),
             'USER': env("DB_USER"),
+            'NAME': env("DB_NAME"),
             'PASSWORD': env("DB_PASSWORD"),
             'HOST': env("DB_HOST"),
             'PORT': env("DB_PORT"),
